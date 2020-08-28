16be2282c0b7ffbff8ea3ebfbca83715723f3a4e
Nyholm <tobias.nyholm@gmail.com>
2020-08-28 21:39:53 +0200

    minior

--- src/Aws/LayerPublisher.php
+++ src/Aws/LayerPublisher.php
@@ -23,44 +23,54 @@ class LayerPublisher
      */
     public function publishLayers(array $layers, array $regions): void
     {
-        $versions = [];
         foreach ($regions as $region) {
-            $result = [];
-            foreach ($layers as $layerName => $layerFilePath) {
-                $result[] = $this->lambda->publishLayerVersion([
-                    '@region' => $region,
-                    'LayerName' => $layerName,
-                    'Description' => $layerName,
-                    'LicenseInfo' => 'MIT',
-                    'CompatibleRuntimes' => [Runtime::PROVIDED],
-                    'Content' => new LayerVersionContentInput(['ZipFile' => file_get_contents($layerFilePath)]),
-                ]);
-            }
+            echo $region.PHP_EOL;
+            $versions = $this->uploadLayers($layers, $region);
+            $this->makeLayersPublic($layers, $region, $versions);
+            echo PHP_EOL;
+        }
+    }
 
-            foreach (Result::wait($result, null, true) as $result) {
-                $versions[$region . $result->getDescription()] = $result->getVersion();
-            }
+    private function uploadLayers(array $layers, string $region): array
+    {
+        $results = [];
+        foreach ($layers as $layerName => $layerFilePath) {
+            $results[] = $this->lambda->publishLayerVersion([
+                '@region' => $region,
+                'LayerName' => $layerName,
+                'Description' => $layerName,
+                'LicenseInfo' => 'MIT',
+                'CompatibleRuntimes' => [Runtime::PROVIDED],
+                'Content' => new LayerVersionContentInput(['ZipFile' => file_get_contents($layerFilePath)]),
+            ]);
         }
 
-        // Add public permissions on the layers
-        foreach ($regions as $region) {
-            $result = [];
-            foreach ($layers as $layerName => $layerFilePath) {
-                $layerVersion = $versions[$region . $layerName];
+        $versions = [];
+        foreach (Result::wait($results, null, true) as $result) {
+            $versions[$region . $result->getDescription()] = $result->getVersion();
+        }
 
-                $result[] = $this->lambda->addLayerVersionPermission([
-                    '@region' => $region,
-                    'LayerName' => $layerName,
-                    'VersionNumber' => $layerVersion,
-                    'StatementId' => 'public',
-                    'Action' => 'lambda:GetLayerVersion',
-                    'Principal' => '*',
-                ]);
-            }
+        return $versions;
+    }
 
-            foreach (Result::wait($result) as $result) {
-                echo '.';
-            }
+    private function makeLayersPublic(array $layers, string $region, array $versions): void
+    {
+        $results = [];
+        foreach ($layers as $layerName => $layerFilePath) {
+            $layerVersion = $versions[$region . $layerName];
+
+            $results[] = $this->lambda->addLayerVersionPermission([
+                '@region' => $region,
+                'LayerName' => $layerName,
+                'VersionNumber' => $layerVersion,
+                'StatementId' => 'public',
+                'Action' => 'lambda:GetLayerVersion',
+                'Principal' => '*',
+            ]);
+        }
+
+        foreach (Result::wait($results) as $result) {
+            echo '.';
         }
     }
 }
