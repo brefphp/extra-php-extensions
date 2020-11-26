f1b747fa016ef84f0b0dc43ab8e805f1c82214af
Nyholm <tobias.nyholm@gmail.com>
2020-11-26 23:58:25 +0100

    Updated test for MongoDB

--- layers/mongodb/test.php
+++ layers/mongodb/test.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!class_exists($class = MongoClient::class)) {
+if (!class_exists($class = MongoDB\Driver\Manager::class)) {
     echo sprintf('FAIL: Class "%s" does not exit.', $class).PHP_EOL;
     exit(1);
 }
