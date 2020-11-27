eeb7282729a1073f59c6c934852a28e23bd1e0a1
Ilya Chekalsky <ilya@chekalsky.com>
2020-11-27 13:57:07 +0100

    Update Imagemagick to version 7 & fix png and jpeg support (#118)
    * Add supported formats check
    
    * Update Imagemagick version
    
    * fix jpeg and png support for imagick

--- layers/imagick/test.php
+++ layers/imagick/test.php
@@ -5,4 +5,11 @@ if (!class_exists($class = Imagick::class)) {
     exit(1);
 }
 
+foreach (['PNG', 'JPG', 'GIF', 'WEBP', 'HEIC'] as $format) {
+    if (!\Imagick::queryFormats($format)) {
+        echo sprintf('FAIL: Imagick does not support "%s".', $format).PHP_EOL;
+        exit(1);
+    }
+}
+
 exit(0);
