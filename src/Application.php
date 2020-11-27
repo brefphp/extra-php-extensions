1eebf81991e57a9679b26fcace3e1de2f22e4c91
Nyholm <tobias.nyholm@gmail.com>
2020-11-27 10:12:20 +0100

    Added a logger

--- src/Application.php
+++ src/Application.php
@@ -9,9 +9,12 @@ use Bref\Extra\Aws\LayerPublisher;
 use Bref\Extra\Command\ListCommand;
 use Bref\Extra\Command\PublishCommand;
 use Bref\Extra\Service\RegionProvider;
+use Bref\Logger\StderrLogger;
 use DI\Container;
 use DI\ContainerBuilder;
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpClient\RetryableHttpClient;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -27,6 +30,9 @@ class Application extends \Silly\Edition\PhpDi\Application
         $builder->addDefinitions([
             'project_dir' => $projectDir,
             'layer_names' => $localLayers,
+            LoggerInterface::class => function (ContainerInterface $c) {
+                return new StderrLogger(LogLevel::INFO);
+            },
             LayerProvider::class => function (ContainerInterface $c) {
                 return new LayerProvider($c->get(LambdaClient::class), $c->get('layer_names'));
             },
@@ -37,12 +43,12 @@ class Application extends \Silly\Edition\PhpDi\Application
                 return new PublishCommand($c->get(LayerPublisher::class), $c->get(RegionProvider::class), $c->get('project_dir'));
             },
             HttpClientInterface::class => function (ContainerInterface $c) {
-                $strategy = new AwsRetryStrategy(AwsRetryStrategy::DEFAULT_RETRY_STATUS_CODES, 5000, 12, 600000);
+                $strategy = new AwsRetryStrategy(AwsRetryStrategy::DEFAULT_RETRY_STATUS_CODES, 5000, 2, 600000);
 
-                return new RetryableHttpClient(HttpClient::create(), $strategy, 2);
+                return new RetryableHttpClient(HttpClient::create(), $strategy, 12, $c->get(LoggerInterface::class));
             },
             LambdaClient::class => function (ContainerInterface $c) {
-                return new LambdaClient([], null, $c->get(HttpClientInterface::class));
+                return new LambdaClient([], null, $c->get(HttpClientInterface::class), $c->get(LoggerInterface::class));
             },
             LayerPublisher::class => function (ContainerInterface $c) {
                 return new LayerPublisher($c->get(LambdaClient::class));
