85dbe4618e6dd48000daea778fa7d9da93f7bd5e
Nyholm <tobias.nyholm@gmail.com>
2020-11-30 09:14:09 +0100

    Fixed SQL Server Library issues

--- layers/sqlsrv/test.php
+++ layers/sqlsrv/test.php
@@ -3,6 +3,7 @@
 exit(0);
 // TODO enable this test
 
+// This test is for sqlsrv.so
 if (!function_exists($func = 'sqlsrv_connect')) {
     echo sprintf('FAIL: Function "%s" does not exit.', $func).PHP_EOL;
     exit(1);
