98d36ebf976178d9fe68aba265ac57efa43511ce
Aaron Breuer <atbreuer11@gmail.com>
2020-12-01 09:43:51 -0500

    Enable sqlsrv tests - Add test for pdo_sqlsrv

--- layers/sqlsrv/test.php
+++ layers/sqlsrv/test.php
@@ -1,11 +1,14 @@
 <?php
 
-exit(0);
-// TODO enable this test
-
 // This test is for sqlsrv.so
 if (!function_exists($func = 'sqlsrv_connect')) {
     echo sprintf('FAIL: Function "%s" does not exit.', $func).PHP_EOL;
+    exit(1);
+}
+
+// This test is for pdo_sqlsrv.so
+if (!extension_loaded('pdo_sqlsrv')) {
+    echo sprintf('FAIL: PDO sqlsrv extension not loaded').PHP_EOL;
     exit(1);
 }
 
