e196817a87af2cf975d223ab10b9dcafd893b18b
Aaron Breuer <atbreuer11@gmail.com>
2021-02-28 15:47:22 -0500

    Updates & fix for sqlsrv extensions (#181)
    * Add sqlsrv extension
    
    * Add sqlsrv instructions and info to Readme
    
    * Fix bug caused by incorrect spacing
    
    * Add ext-sqlsrv.ini
    - Update Readme
    
    * Enable sqlsrv tests
    - Add test for pdo_sqlsrv
    
    * Fix driver path in odbcinst.ini
    - Update unixODBC & source from microsoft
    - Remove old libs
    - Add test case for drivers (from brefphp/extra-php-extensions#179)
    
    * Add ODBCSYSINI as env variable to sqlsrv/test.php
    
    * Remove generation of ini files, copy from /etc instead
    
    * Remove mssql-tools
    
    * Fix sqlsrv test
    - Install unixODBC-devel from amaz2-core instead of microsoft's rhel repo
    
    * Remove version from unixODBC-devel since amazon linux 1 & 2 have different versions
    
    * Revert "Remove version from unixODBC-devel since amazon linux 1 & 2 have different versions"
    
    This reverts commit dc340e15d39365863a8847cd7eac58a98037850e.

--- layers/sqlsrv/test.php
+++ layers/sqlsrv/test.php
@@ -1,5 +1,7 @@
 <?php
 
+putenv('ODBCSYSINI=/opt/microsoft/conf/');
+
 // This test is for sqlsrv.so
 if (!function_exists($func = 'sqlsrv_connect')) {
     echo sprintf('FAIL: Function "%s" does not exist.', $func).PHP_EOL;
@@ -10,6 +12,16 @@ if (!function_exists($func = 'sqlsrv_connect')) {
 if (!extension_loaded('pdo_sqlsrv')) {
     echo sprintf('FAIL: PDO sqlsrv extension not loaded').PHP_EOL;
     exit(1);
+}
+
+// This test attempts a connection, forcing MS odbc libraries to be loaded
+if (!sqlsrv_connect('localhost', ['LoginTimeout' => 1])) {
+    foreach (sqlsrv_errors() as $error) {
+        if ($error['SQLSTATE'] == '01000') {
+            echo sprintf('FAIL: sqlsrv extension library not loaded. %s', $error['message']).PHP_EOL;
+            exit(1);
+        }
+    }
 }
 
 exit(0);
