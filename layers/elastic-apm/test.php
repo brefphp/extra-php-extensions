5bdc7614e9f699b725e265a6d6c130f5772476b4
Matt Renner <matt@rennernz.com>
2022-03-17 15:13:02 +1300

    Reduce inclusions to bare minimum - users can import required ElasticApm classes in their own application

--- layers/elastic-apm/test.php
+++ layers/elastic-apm/test.php
@@ -6,10 +6,4 @@ if (!function_exists($func = 'elastic_apm_send_to_server')) {
     exit(1);
 }
 
-// Provided by the included PHP package
-if (!method_exists($class = 'ElasticApm', $method = 'beginCurrentTransaction')) {
-    echo sprintf('FAIL: Method "%s::%s" does not exist.', $class, $method) . PHP_EOL;
-    exit(1);
-}
-
 exit(0);
