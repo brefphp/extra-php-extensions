a476da19331c0aa73f6283fc348f7e79cf0dcc00
Mārtiņš <liepumartins@users.noreply.github.com>
2022-11-03 14:13:51 +0200

    Correct fully qualified name reference

--- layers/decimal/test.php
+++ layers/decimal/test.php
@@ -1,12 +1,12 @@
 <?php
 
-if (!class_exists($class = Decimal::class)) {
+if (!class_exists($class = Decimal\Decimal::class)) {
     echo sprintf('FAIL: Class "%s" does not exist.', $class).PHP_EOL;
     exit(1);
 }
 
 try {
-    $d = new \Decimal(5);
+    $d = new \Decimal\Decimal(5);
     $d->isZero();
 } catch (\Throwable $e) {
     echo sprintf('FAIL: Decimal failed with "%s" exception: %s', get_class($e), $e->getMessage()).PHP_EOL;
